ee8a4de8400776cc78099c1eacd6d924953b5c43
Luiz Almeida <luizalmeida@alunos.utfpr.edu.br>
2020-09-24 18:58:57 -0300

    Alteração:
    - Adição de matriz;
    - Criação de função para alterar a matriz conforme adiciona os discos;

--- script2.js
+++ script2.js
@@ -1,5 +1,14 @@
+let tabuleiroMatriz = [
+    [" ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " "],
+];
+
 let count = 0
-const mainFunction = function (event) {
+const mainFunction = (event) => {
     let column = event.currentTarget
     let child = column.childElementCount
     if (child < 6) {
@@ -8,14 +17,54 @@ const mainFunction = function (event) {
             disco.className = 'discosColunas'
             disco.classList.add('discoClaro')
             column.appendChild(disco)
-            count += 1
+            addMatriz(column, child, "R")
+            console.log(tabuleiroMatriz)
+            count++
         } else {
             let disco = document.createElement('div')
             disco.className = 'discosColunas'
             disco.classList.add('discoEscuro')
+            addMatriz(column, child, "P")
+            console.log(tabuleiroMatriz)
             column.appendChild(disco)
-            count += 1
+            count++
         }
+    }
+}
+
+function addMatriz (column, child, disc) {
+    // for (let y = 0; y < column.length; y++) {
+    //     if (column.id === `column${y}`) {
+    //         tabuleiroMatriz[5 - column.childElementCount][y] = disc;
+    //         console.log(tabuleiroMatriz[5 - column.childElementCount][y]);
+    //     }
+    // }
+    console.log(column.id)
+
+    switch (column.id) {
+        case 'column0':
+            tabuleiroMatriz[5 - child][0] = disc;
+            break;
+        case 'column1':
+            tabuleiroMatriz[5 - child][1] = disc;
+            break;
+        case 'column2':
+            tabuleiroMatriz[5 - child][2] = disc;
+            break;
+        case 'column3':
+            tabuleiroMatriz[5 - child][3] = disc;
+            break;
+        case 'column4':
+            tabuleiroMatriz[5 - child][4] = disc;
+            break;
+        case 'column5':
+            tabuleiroMatriz[5 - child][5] = disc;
+            break;
+        case 'column6':
+            tabuleiroMatriz[5 - child][6] = disc;
+            break;
+        default:
+            break;
     }
 }
 
