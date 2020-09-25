ea72810eeacbddf56ff6fc7b596f0d4f7d7c7072
Cirineu Rodrigues <cirineurodrigues@hotmail.com>
2020-09-24 21:58:14 -0300

    Inclui peça atual, e condições de vitória na horizontal e vertical

--- script2.js
+++ script2.js
@@ -1,3 +1,5 @@
+//Variáveis de escopo global
+let count = 0
 let tabuleiroMatriz = [
     [" ", " ", " ", " ", " ", " ", " "],
     [" ", " ", " ", " ", " ", " ", " "],
@@ -7,40 +9,51 @@ let tabuleiroMatriz = [
     [" ", " ", " ", " ", " ", " ", " "],
 ];
 
-let count = 0
-const mainFunction = (event) => {
-    let column = event.currentTarget
-    let child = column.childElementCount
-    if (child < 6) {
-        if (count % 2 === 0) {
-            let disco = document.createElement('div')
-            disco.className = 'discosColunas'
-            disco.classList.add('discoClaro')
-            column.appendChild(disco)
-            addMatriz(column, child, "R")
-            console.log(tabuleiroMatriz)
-            count++
-        } else {
-            let disco = document.createElement('div')
-            disco.className = 'discosColunas'
-            disco.classList.add('discoEscuro')
-            addMatriz(column, child, "P")
-            console.log(tabuleiroMatriz)
-            column.appendChild(disco)
-            count++
+//Inicia a cor da peça atual como a mais clara
+let getDivPecaAtual = document.getElementById('pecaAtual')
+getDivPecaAtual.style.backgroundColor = '#855bcc'
+
+//Função que chega as possíveis condições de vitória
+function win() {
+    let getContainer = document.getElementById('container')
+    let diskCounter = 0
+    //Verifica a vitória na horizontal
+    for (let i = 0; i < tabuleiroMatriz.length; i++) {
+        for (let j = 0; j < tabuleiroMatriz[i].length - 1; j++) {
+            if (tabuleiroMatriz[i][j] !== ' ' && (tabuleiroMatriz[i][j] === tabuleiroMatriz[i][j + 1])) {
+                diskCounter += 1
+            } else {
+                diskCounter = 0
+            }
+            if (diskCounter === 3) {
+                console.log('acabou')
+                getContainer.innerHTML = ''
+                getContainer.innerText = 'Ganhou!'
+            }
         }
     }
+
+    //Verifica a vitória na vertical
+    for (let i = 0; i < tabuleiroMatriz[0].length; i++) {
+        for (let j = 0; j < tabuleiroMatriz.length - 1; j++) {
+            if (tabuleiroMatriz[j][i] !== ' ' && (tabuleiroMatriz[j][i] === tabuleiroMatriz[j + 1][i])) {
+                diskCounter += 1
+            } else {
+                diskCounter = 0
+            }
+            if (diskCounter === 3) {
+                console.log('acabou')
+                getContainer.innerHTML = ''
+                getContainer.innerText = 'Ganhou!'
+            }
+        }
+    }
+
+    //Verifica a vitória na diagonal 
 }
 
-function addMatriz (column, child, disc) {
-    // for (let y = 0; y < column.length; y++) {
-    //     if (column.id === `column${y}`) {
-    //         tabuleiroMatriz[5 - column.childElementCount][y] = disc;
-    //         console.log(tabuleiroMatriz[5 - column.childElementCount][y]);
-    //     }
-    // }
-    console.log(column.id)
-
+//Função que preenche a matriz
+function addMatriz(column, child, disc) {
     switch (column.id) {
         case 'column0':
             tabuleiroMatriz[5 - child][0] = disc;
@@ -63,9 +76,32 @@ function addMatriz (column, child, disc) {
         case 'column6':
             tabuleiroMatriz[5 - child][6] = disc;
             break;
-        default:
-            break;
     }
+}
+
+//Função principal do jogo, faz a criação dos discos, revezamento dos jogadores, preenchimento da matriz, chega as condições de vitória e empate
+const mainFunction = (event) => {
+    const disco = document.createElement('div')
+    const column = event.currentTarget
+    let child = column.childElementCount
+    if (child < 6) {
+        if (count % 2 === 0) {
+            disco.className = 'discosColunas'
+            disco.classList.add('discoClaro')
+            column.appendChild(disco)
+            addMatriz(column, child, "R")
+            getDivPecaAtual.style.backgroundColor = '#01141D'
+            count++
+        } else {
+            disco.className = 'discosColunas'
+            disco.classList.add('discoEscuro')
+            addMatriz(column, child, "P")
+            column.appendChild(disco)
+            getDivPecaAtual.style.backgroundColor = '#855bcc'
+            count++
+        }
+    }
+    win()
 }
 
 //eventListeners
